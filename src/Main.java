5865bf04d96a1a39ce48ccec91e7e95e558f26eb
ljgreenhill <ljgreenhill@yahoo.com>
2018-02-05 14:46:11 -0500

    added Robot class

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,17 @@
 
 public class Main {
 	
+	final static long MATCH_TIME = 135;
+	
+	
 	public static void main(String[] args) {
 		
+		
 		FrontEnd myFrontEnd = new FrontEnd();
+		
+		
+		
+		
 	}
 
 }
---
+++ src/Robot.java
@@ -0,0 +1,62 @@
+
+public class Robot {
+	
+
+	double switchTimeDefense;
+	double switchTimeNeutral;
+	double scaleTimeDefense;
+	double scaleTimeNeutral;
+	double climbTime;
+	double exchangeTime;
+	double vaultTime;
+	
+	public double getSwitchTimeDefense() {
+		return switchTimeDefense;
+	}
+	
+	public void setSwitchTimeDefense(double switchTimeDefense) {
+		this.switchTimeDefense = switchTimeDefense;
+	}
+	
+	public double getSwitchTimeNeutral() {
+		return switchTimeNeutral;
+	}
+	
+	public void setSwitchTimeNeutral(double switchTimeNeutral) {
+		this.switchTimeNeutral = switchTimeNeutral;
+	}
+	
+	
+	public double getScaleTimeDefense() {
+		return scaleTimeDefense;
+	}
+	
+	public void setScaleTimeDefense(double scaleTimeDefense) {
+		this.scaleTimeDefense = scaleTimeDefense;
+	}
+	
+	public double getScaleTimeNeutral() {
+		return scaleTimeNeutral;
+	}
+	
+	public void setScaleTimeNeutral(double scaleTimeNeutral) {
+		this.scaleTimeNeutral = scaleTimeNeutral;
+	}
+	
+	public double getClimbTime() {
+		return climbTime;
+	}
+	
+	public void setClimbTime(double climbTime) {
+		this.climbTime = climbTime;
+	}
+	
+	public double getExchangeTime() {
+		return exchangeTime;
+	}
+	
+	public void setExchangeTime(double exchangeTime) {
+		this.exchangeTime = exchangeTime;
+	}
+
+}
